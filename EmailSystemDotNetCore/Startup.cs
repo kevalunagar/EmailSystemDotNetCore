73791dd74b619b649dcf6e9ef7187f7d9217bec0
Smit3343 <smitvaghani3343@gmail.com>
2021-10-30 16:48:02 +0530

    bootstrap

--- EmailSystemDotNetCore/Startup.cs
+++ EmailSystemDotNetCore/Startup.cs
@@ -1,6 +1,10 @@
+using EmailSystemDotNetCore.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,11 +16,21 @@ namespace EmailSystemDotNetCore
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration _configuration)
+        {
+            configuration = _configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("EmailSystemCon")));
+            services.AddIdentity<UserModel, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();
             services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,7 +41,9 @@ namespace EmailSystemDotNetCore
                 app.UseDeveloperExceptionPage();
             }
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseRouting();
+
 
             app.UseEndpoints(endpoints =>
             {
