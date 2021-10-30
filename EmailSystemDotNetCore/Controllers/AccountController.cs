b7ba3d2697f9d1530c55a59bc5573820e143ee49
Smit3343 <smitvaghani3343@gmail.com>
2021-10-30 15:32:43 +0530

    initial commit

---
+++ EmailSystemDotNetCore/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailSystemDotNetCore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
